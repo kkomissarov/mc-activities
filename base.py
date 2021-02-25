a2f0059654a6f6a3f59e834e1ae1962024acc92d
Konstantin Komissarov <hello@kkomissarov.ru>
2021-02-25 22:44:43 +0300

    added getting db credentials from config

--- base.py
+++ base.py
@@ -1,12 +1,12 @@
 import os
 
+import config
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
-DB_HOST = os.environ.get('WS_STAGE_HOST')
-
-engine = create_engine(f'postgresql://postgres:postgres@{DB_HOST}:5432/smart_space_test')
+db_string = f'postgresql://{config.DB_USER}:{config.DB_PASS}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}'
+engine = create_engine(db_string)
 
 Base = declarative_base(bind=engine)
 metadata = MetaData(bind=engine)
